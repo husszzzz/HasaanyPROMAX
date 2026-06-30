a02342a8ba89476d3ba9d8bdcbf2b960be3ec385
hassanyIPA <hussien2010rtx@gmail.com>
2026-07-01 00:38:26 +0300

    Implement XML response handler with input validation

--- api/install.js
+++ api/install.js
@@ -1,1 +1,52 @@
+export default function handler(req, res) {
+    const { name, url, bid } = req.query;
 
+    if (!name || !url || !bid) {
+        return res.status(400).send('Missing parameters');
+    }
+
+    // تنظيف البيانات لضمان توافق XML
+    const clean = (str) => str.replace(/[<>&'"]/g, c => ({
+        '<': '&lt;', '>': '&gt;', '&': '&amp;', "'": '&apos;', '"': '&quot;'
+    }[c]));
+
+    const safeName = clean(name);
+    const safeUrl = clean(url);
+    const safeBid = clean(bid);
+
+    const plist = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>items</key>
+    <array>
+        <dict>
+            <key>assets</key>
+            <array>
+                <dict>
+                    <key>kind</key>
+                    <string>software-package</string>
+                    <key>url</key>
+                    <string>${safeUrl}</string>
+                </dict>
+            </array>
+            <key>metadata</key>
+            <dict>
+                <key>bundle-identifier</key>
+                <string>${safeBid}</string>
+                <key>bundle-version</key>
+                <string>1.0</string>
+                <key>kind</key>
+                <string>software</string>
+                <key>title</key>
+                <string>${safeName}</string>
+            </dict>
+        </dict>
+    </array>
+</dict>
+</plist>`;
+
+    res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+    res.status(200).send(plist);
+}
